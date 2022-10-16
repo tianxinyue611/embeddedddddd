cd89899f23c6533066cc6fb4e6c1e4c3e3f3d0e7
tianxinyue611 <104822349+tianxinyue611@users.noreply.github.com>
2022-10-16 12:20:11 -0400

    Add files via upload

---
+++ blink.py
@@ -0,0 +1,15 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)   # Set for GPIO numbering not pin numbers...
+GPIO.setup(13, GPIO.OUT) # set GPIO 13 as output to blink LED
+
+i = 0
+while i < 10000000:
+    GPIO.output(13, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(13, GPIO.LOW)
+    time.sleep(0.5)
+    i = i + 1
+
+GPIO.cleanup()
